2a7f0df2ada6ba4fc650007af9b04d0c6f0cda33
masrifadel <masrifadel68@gmail.com>
2026-03-31 00:02:16 +0300

    Trigger deployment - fix MongoDB connection

--- src/server.js
+++ src/server.js
@@ -89,6 +89,7 @@ process.on("SIGINT", () => {
   process.exit(0);
 });
 
+// Deployment trigger
 const server = app.listen(PORT, () => {
   console.log(
     `Server running in ${process.env.NODE_ENV || "development"} mode on port ${PORT}`,
